e2c552aa41aafad8812d146e96df14ea028bb8d9
LeoDataEngineer <116231124+LeoDataEngineer@users.noreply.github.com>
2024-02-28 20:07:17 -0300

    Update extract.py

--- scripts/extract.py
+++ scripts/extract.py
@@ -4,7 +4,7 @@ import pandas as pd
 def get_data():
    endpoint_subte = "https://apitransporte.buenosaires.gob.ar/subtes/forecastGTFS?client_id=eeea2fd521514498a37629a810012185&client_secret=14C6598f35E2498185685Ccfc6b2b372"
    
-    # endpoint_subte= "https://apitransporte.buenosaires.gob.ar/subtes/forecastGTFS?client_id={{ secrets.CLIENT_ID }}&client_secret={{ secrets.CLIENT_SECRET }}}"
+  
    
     try:
         response = requests.get(endpoint_subte)
